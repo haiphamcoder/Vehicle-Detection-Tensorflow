69ae8b55cd32779ac02781a7c298214c991972f8
Hai Pham <ngochai285nd@gmail.com>
2022-12-14 10:03:32 +0700

    update

--- MainDashboard.py
+++ MainDashboard.py
@@ -1,5 +1,5 @@
 # This Python file uses the following encoding: utf-8
-from PyQt5.QtWidgets import QMainWindow, QHeaderView
+from PyQt5.QtWidgets import QMainWindow, QHeaderView, QButtonGroup
 from PyQt5.QtCore import QFile, QTimer, QDateTime, QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtMultimedia import QCameraInfo
 from PyQt5.QtGui import QStandardItemModel, QImage, QPixmap
@@ -50,8 +50,10 @@ class MainDashboard(QMainWindow):
         self.btnSelect.setEnabled(False)
         self.pathVideoFile.setEnabled(False)
 
-        self.rdbCamera.toggled.connect(lambda:self.buttonState(self.rdbCamera))
-        self.rdbVideoFile.toggled.connect(lambda:self.buttonState(self.rdbVideoFile))
+        self.rdbCamera.toggled.connect(
+            lambda: self.buttonState(self.rdbCamera))
+        self.rdbVideoFile.toggled.connect(
+            lambda: self.buttonState(self.rdbVideoFile))
 
         self.actionQuit.triggered.connect(self.exit_program)
         self.btnClose.clicked.connect(self.exit_program)
@@ -62,26 +64,40 @@ class MainDashboard(QMainWindow):
 
     def getCpuUsage(self, cpu):
         self.lblCPU.setText(str(cpu) + "%")
-        if cpu > 15: self.lblCPU.setStyleSheet("color: rgb(23, 63, 95);")
-        if cpu > 25: self.lblCPU.setStyleSheet("color: rgb(32, 99, 155);")
-        if cpu > 45: self.lblCPU.setStyleSheet("color: rgb(60, 174, 163);")
-        if cpu > 65: self.lblCPU.setStyleSheet("color: rgb(246, 213, 92);")
-        if cpu > 85: self.lblCPU.setStyleSheet("color: rgb(237, 85, 59);")
+        if cpu > 15:
+            self.lblCPU.setStyleSheet("color: rgb(23, 63, 95);")
+        if cpu > 25:
+            self.lblCPU.setStyleSheet("color: rgb(32, 99, 155);")
+        if cpu > 45:
+            self.lblCPU.setStyleSheet("color: rgb(60, 174, 163);")
+        if cpu > 65:
+            self.lblCPU.setStyleSheet("color: rgb(246, 213, 92);")
+        if cpu > 85:
+            self.lblCPU.setStyleSheet("color: rgb(237, 85, 59);")
 
     def getRamUsage(self, ram):
         self.lblRAM.setText(str(ram[2]) + "%")
-        if ram[2] > 15: self.lblRAM.setStyleSheet("color: rgb(23, 63, 95);")
-        if ram[2] > 25: self.lblRAM.setStyleSheet("color: rgb(32, 99, 155);")
-        if ram[2] > 45: self.lblRAM.setStyleSheet("color: rgb(60, 174, 163);")
-        if ram[2] > 65: self.lblRAM.setStyleSheet("color: rgb(246, 213, 92);")
-        if ram[2] > 85: self.lblRAM.setStyleSheet("color: rgb(237, 85, 59);")
+        if ram[2] > 15:
+            self.lblRAM.setStyleSheet("color: rgb(23, 63, 95);")
+        if ram[2] > 25:
+            self.lblRAM.setStyleSheet("color: rgb(32, 99, 155);")
+        if ram[2] > 45:
+            self.lblRAM.setStyleSheet("color: rgb(60, 174, 163);")
+        if ram[2] > 65:
+            self.lblRAM.setStyleSheet("color: rgb(246, 213, 92);")
+        if ram[2] > 85:
+            self.lblRAM.setStyleSheet("color: rgb(237, 85, 59);")
 
     def getFPS(self, fps):
         self.lblFPS.setText(str(fps))
-        if fps > 5: self.lblFPS.setStyleSheet("color: rgb(237, 85, 59);")
-        if fps > 15: self.lblFPS.setStyleSheet("color: rgb(60, 174, 155);")
-        if fps > 25: self.lblFPS.setStyleSheet("color: rgb(85, 170, 255);")
-        if fps > 35: self.lblFPS.setStyleSheet("color: rgb(23, 63, 95);")
+        if fps > 5:
+            self.lblFPS.setStyleSheet("color: rgb(237, 85, 59);")
+        if fps > 15:
+            self.lblFPS.setStyleSheet("color: rgb(60, 174, 155);")
+        if fps > 25:
+            self.lblFPS.setStyleSheet("color: rgb(85, 170, 255);")
+        if fps > 35:
+            self.lblFPS.setStyleSheet("color: rgb(23, 63, 95);")
 
     def buttonState(self, button):
         if button.text() == "Camera":
@@ -116,7 +132,8 @@ class MainDashboard(QMainWindow):
 
         h, w, ch = rgb_img.shape
         bytes_per_line = ch * w
-        cvt2QtFormat = QImage(rgb_img.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        cvt2QtFormat = QImage(rgb_img.data, w, h,
+                              bytes_per_line, QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(cvt2QtFormat)
 
         return pixmap  # QPixmap.fromImage(cvt2QtFormat)
@@ -137,7 +154,6 @@ class MainDashboard(QMainWindow):
             self.worker.fps.connect(self.getFPS)
             self.worker.start()
 
-
         except Exception as error:
             pass
 
@@ -153,6 +169,7 @@ class MainDashboard(QMainWindow):
         self.ThreadActive = False
         sys.exit()
 
+
 class SystemMonitor(QThread):
     cpu = pyqtSignal(float)
     ram = pyqtSignal(tuple)
@@ -164,6 +181,7 @@ class SystemMonitor(QThread):
             ram = psutil.virtual_memory()
             self.cpu.emit(cpu)
             self.ram.emit(ram)
+
     def stop(self):
         self.ThreadActive = False
         self.quit()
